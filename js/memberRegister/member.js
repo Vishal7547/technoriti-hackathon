266c93e7ae6ae23f9bf67d9a59eb4ef869239891
Vishal kumar <vishalk80269@gmail.com>
2023-03-01 22:57:50 +0530

    testing mode

---
+++ js/memberRegister/member.js
@@ -0,0 +1,76 @@
+const submitForm=document.getElementById('submitForm');
+
+const memberOfClub = window.localStorage.getItem("members");
+let member = memberOfClub ? JSON.parse(memberOfClub)  : [];
+
+
+function addEventTask(x){
+    if(x){
+        member.push(x);
+        window.localStorage.setItem("members", JSON.stringify(member));
+        showNotification('register successfully ');
+        // window.open('event.html');
+        console.log(member);
+        return;
+    }
+    showNotification('something wrong');
+}
+
+
+
+function insertArray(obj){
+    const member={
+        name:obj.name,
+        event:obj.event,
+        college:obj.college,
+        year:obj.year,
+        branch:obj.branch,
+        password:obj.password,
+         id:Date.now(),
+         mobile:obj.mobile,
+        
+         
+    
+     }
+   addEventTask(member);
+  
+    //  console.log(task);
+}
+
+
+submitForm.addEventListener('click',function(){
+   
+    const name=document.getElementById('name').value;
+    const college=document.getElementById('college').value;
+    const year=document.getElementById('year').value;
+    const branch=document.getElementById('branch').value;
+    const password=document.getElementById('Password').value;
+    const mobile=document.getElementById('mobile').value;
+    if(name == "" || college=="" || year == ""  || branch == ""  || mobile == "" || password==""){
+        showNotification('box can not be empty');
+    
+        return;
+    }
+    
+   
+    let obj={
+      
+        name,
+        college,
+        year,
+        branch,
+        password,
+        mobile,
+       
+}
+console.log(obj);
+insertArray(obj);
+
+})
+
+
+
+
+function showNotification(e){
+    alert(e);
+}
